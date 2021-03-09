80d2a188d52fb8b2b837ffe394f139f4c103abf3
kevin park <kevin.park02@gmail.com>
2021-03-09 09:55:00 -0800

    Refactor the entry to point to have the rootjsx

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from './store/store';
+import Root from "./components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
-  const root = document.getElementById("root");
-
+  
   // Testing - Start
   window.getState = store.getState;
   window.dispatch = store.dispatch;
   // Testing - End
-  ReactDOM.render(<h1>Welcome to Bible Equipping</h1>, root);
+
+  const root = document.getElementById("root");
+  ReactDOM.render(<Root store={store} />, root);
 });
