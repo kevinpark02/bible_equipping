16289836ad253bf73c9c5e3f3093076e636bdb5c
kevin park <kevin.park02@gmail.com>
2021-03-08 23:53:39 -0800

    Add validation and auth methods to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,57 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
+    # VALIDATIONS - START
+    validates :email, :username, :password_digest, :session_token, presence: true
+    validates :email, :username, uniqueness: true
+    validates :password, length: {minimum: 8, allow_nil: true}
+    # VALIDATIONS - END
+
+    # AUTHENTIFICATION METHODS - START
+    attr_reader :password
+    after_initialize :ensure_session_token
+    
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    private
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
+    # AUTHENTIFICATION METHODS - END
+
+
 end
