2f3e7d6aada4c37a06ee83ec2dec89b7539d6b20
kevin park <kevin.park02@gmail.com>
2021-03-23 00:34:05 -0700

    Add validations and associations for the verse model

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -16,4 +16,9 @@ class Quiz < ApplicationRecord
         primary_key: :id,
         foreign_key: :user_id,
         class_name: :User
+
+    has_many :verses,
+        primary_key: :id,
+        foreign_key: :quiz_id,
+        class_name: :Verse
 end
--- app/models/verse.rb
+++ app/models/verse.rb
@@ -10,4 +10,10 @@
 #  updated_at :datetime         not null
 #
 class Verse < ApplicationRecord
+    validates :verse, :chapter, :quiz_id, presence: true
+
+    belongs_to :quiz,
+        primary_key: :id,
+        foreign_key: :quiz_id,
+        class_name: :Quiz
 end
