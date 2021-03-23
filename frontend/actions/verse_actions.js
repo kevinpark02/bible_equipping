07a572c1f7fd45289ec0259f74b7c3190a919986
kevin park <kevin.park02@gmail.com>
2021-03-23 01:37:16 -0700

    Make action creators for verse

---
+++ frontend/actions/verse_actions.js
@@ -0,0 +1,15 @@
+import * as VerseApiUtil from "../util/verse_api_util";
+
+export const RECEIVE_VERSE = "RECEIVE_VERSE";
+
+export const receiveVerse = (verse) => {
+    return{
+        type: RECEIVE_VERSE,
+        verse
+    };
+};
+
+export const createVerse = (verse) => (dispatch) => {
+    return VerseApiUtil.createVerse(verse)
+        .then((verse) => dispatch(receiveVerse(verse)));
+};
