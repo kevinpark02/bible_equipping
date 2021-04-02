44cfce6baa50719fa3947a9dfa0b69d26721ee04
kevin park <kevin.park02@gmail.com>
2021-04-01 20:50:51 -0700

    Make loading icon

---
+++ frontend/components/verse/loading_icon.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LoadingIcon = () => (
+    <div id="loading-bible-container">
+        <div id="loading-bible"></div>
+    </div>
+);
+
+export default LoadingIcon;
