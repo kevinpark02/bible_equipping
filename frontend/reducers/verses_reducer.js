16b6f42ff4a48e25a3ff8f9dfea95d506416c002
kevin park <kevin.park02@gmail.com>
2021-04-01 17:29:01 -0700

    Clear the verses state when going back to the homepage

--- frontend/reducers/verses_reducer.js
+++ frontend/reducers/verses_reducer.js
@@ -1,22 +1,22 @@
 import { RECEIVE_VERSE } from '../actions/verse_actions';
 import { RECEIVE_CURRENT_QUIZ } from "../actions/quiz_actions";
-import { LOGOUT_CURRENT_USER } from "../actions/session_actions";
+import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from "../actions/session_actions";
 
 const versesReducer = (state = {}, action) => {
     Object.freeze(state);
     let nextState = Object.assign({}, state);
     switch(action.type) {
         case RECEIVE_VERSE:
-            // debugger
             nextState[action.verse.id] = action.verse
             return nextState
-            // return Object.assign(nextState, action.verse)
         case RECEIVE_CURRENT_QUIZ:
             if (action.quiz.verses === undefined) {
                 return {}
             } else {
                 return action.quiz.verses
             }
+        case RECEIVE_CURRENT_USER:
+            return {}
         case LOGOUT_CURRENT_USER:
             return {}
         default:
