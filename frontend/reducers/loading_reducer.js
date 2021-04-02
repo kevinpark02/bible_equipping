a796212a31855f93960bab52f72fcbeccd530e46
kevin park <kevin.park02@gmail.com>
2021-04-01 20:59:12 -0700

    Making loading reducer

---
+++ frontend/reducers/loading_reducer.js
@@ -0,0 +1,20 @@
+import { RECEIVE_VERSE, START_RECEIVE_VERSE } from "../actions/verse_actions";
+
+const initialState = {
+    verseMaking: false
+};
+
+const loadingReducer = (state = initialState, action) => {
+    Object.freeze(state);
+
+    switch (action.type) {
+        case RECEIVE_VERSE:
+            return Object.assign({}, state, { verseMaking: false });
+        case START_RECEIVE_VERSE:
+            return Object.assign({}, state, { verseMaking: true });
+        default:
+            return state;
+    }
+};
+
+export default loadingReducer;
