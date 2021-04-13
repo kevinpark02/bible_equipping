1bbcd1514cd26f88688221552eab1f2cf83ce6fe
kevin park <kevin.park02@gmail.com>
2021-04-12 21:00:23 -0700

    Create collection controller

--- app/controllers/api/collections_controller.rb
+++ app/controllers/api/collections_controller.rb
@@ -1,2 +1,19 @@
 class Api::CollectionsController < ApplicationController
+    before_action :require_logged_in, only: [:create]
+
+    def create
+        @collection = Collection.new(collection_params)
+
+        if @collection.save
+            render :show
+        else
+            render json: @quiz.errors.full_messages, status: 422
+        end
+    end
+
+    private
+
+    def collection_params
+        params.require(:collection).permit(:book)
+    end
 end
