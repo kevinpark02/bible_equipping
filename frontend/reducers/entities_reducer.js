570506e4898c2226a7a52243aed4894094a26e3e
kevin park <kevin.park02@gmail.com>
2021-04-16 11:18:33 -0700

    Combine collections reducer to entities

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import usersReducer from "./users_reducer";
 import quizzesReducer from "./quizzes_reducer";
 import versesReducer from "./verses_reducer";
+import collectionsReducer from "./collections_reducer";
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
+  collection: collectionsReducer,
   quizzes: quizzesReducer,
   verses: versesReducer,
 });
