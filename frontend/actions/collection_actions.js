8e364b944c99c703274f9f3216613db37ebcfab2
kevin park <kevin.park02@gmail.com>
2021-04-16 10:33:16 -0700

    Write action creators for fetching a collection

--- frontend/actions/collection_actions.js
+++ frontend/actions/collection_actions.js
@@ -1,6 +1,7 @@
 import * as CollectionApiUtil from "../util/collection_api_util";
 
 export const RECEIVE_COLLECTION = "RECEIVE_COLLECTION";
+export const RECEIVE_CURRENT_COLLECTION = "RECEIVE_CURRENT_COLLECTION";
 
 export const receiveCollection = (collection) => {
     return {
@@ -9,9 +10,22 @@ export const receiveCollection = (collection) => {
     };
 };
 
+export const receiveCurrentCollection = (collection) => {
+    return {
+        type: RECEIVE_CURRENT_COLLECTION,
+        collection
+    };
+};
+
 export const createCollection = (collection) => (dispatch) => {
     return CollectionApiUtil.createCollection(collection)
         .then((collection) => dispatch(receiveCollection(collection))
+    );
+};
+
+export const fetchCollection = (collectionId) => (dispatch) => {
+    return CollectionApiUtil.fetchCollection(collectionId)
+        .then((collection) => dispatch(receiveCurrentCollection(colection))
     );
 };
 
