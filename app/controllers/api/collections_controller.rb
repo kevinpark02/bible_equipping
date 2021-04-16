c051ef31cdb3fc42e1b8888e6691a3ee355d34ba
kevin park <kevin.park02@gmail.com>
2021-04-16 09:06:00 -0700

    Add show action to the collection controller

--- app/controllers/api/collections_controller.rb
+++ app/controllers/api/collections_controller.rb
@@ -1,5 +1,9 @@
 class Api::CollectionsController < ApplicationController
-    before_action :require_logged_in, only: [:create]
+    before_action :require_logged_in, only: [:show, :create]
+
+    def show
+        @collection = Collection.includes(:quizzes).find_by(id: params[:id])
+    end
 
     def create
         @collection = Collection.new(collection_params)
@@ -14,6 +18,6 @@ class Api::CollectionsController < ApplicationController
     private
 
     def collection_params
-        params.require(:collection).permit(:book)
+        params.require(:collection).permit(:book, :user_id)
     end
 end
